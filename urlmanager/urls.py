fc300f178ef380cc909d196175cdefb733a41965
Den Kasyanov <den@den.ai>
2018-04-15 09:51:09 +0300

    Add view for redirecting to existing full urls or adding new ones if short path does not exist

--- urlmanager/urls.py
+++ urlmanager/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from .views import AddUrl
+from .views import AddUrl, ShortUrlRedirect
 
 
 app_name = 'urlmanager'
 
 urlpatterns = [
     path('', AddUrl.as_view(), name='add_url'),
+    path('<short_url_path>/', ShortUrlRedirect.as_view(), name='redirect'),
 ]
--- urlmanager/views.py
+++ urlmanager/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 from django.views.generic.edit import FormView
 
 from .models import Url
@@ -15,3 +16,21 @@ class AddUrl(FormView):
         full_url = form.cleaned_data['full_url']
         obj, created = Url.objects.get_or_create(full_url=full_url)
         return render(self.request, 'urlmanager/display_short_url.html', {'short_url': obj.get_short_url()})
+
+class ShortUrlRedirect(View):
+
+    def get(self, request, *args, **kwargs):
+        short_url_path = kwargs['short_url_path']
+        if short_url_path:
+            try:
+                url = Url.objects.get(short_url_path=short_url_path)
+                if url is not None:
+                    return redirect(url.get_full_url())
+            except Url.DoesNotExist:
+                # In order to prevent abuse, additional logic can be added here.
+                # For example counting unsuccessful trials.
+                # For now we'll go with silently redirecting to add url page.
+                pass
+        # If nonexistent short url or even arbitrary path is used,
+        # silently redirect to page for adding URL
+        return redirect('urlmanager:add_url')

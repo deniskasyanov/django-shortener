729a9aadd65b1fd3845fd093fd7f892ac2253b83
Den Kasyanov <den@den.ai>
2018-04-15 15:23:33 +0300

    Make Add url page look neat

--- urlmanager/forms.py
+++ urlmanager/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.forms.widgets import TextInput
 
 from .models import Url
 
@@ -6,6 +7,16 @@ class AddUrl(ModelForm):
     class Meta:
         model = Url
         fields = ['full_url',]
+
+        widgets = {
+            'full_url': TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter URL'})
+        }
+
+        labels = {
+            'full_url': '',
+        }
+
+
 
     def clean_full_url(self):
         full_url = self.cleaned_data['full_url']
